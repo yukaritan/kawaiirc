4bedfcbd6f1c36242facd323cb0505eb42131e85
yukaritan <yukaritan@users.noreply.github.com>
2014-09-20 23:39:45 +0200

    - Added some comments, generated some docs

--- util/dictable.py
+++ util/dictable.py
@@ -3,12 +3,18 @@ from builtins import classmethod, AttributeError, getattr, dir
 
 class Dictable:
     """
-    Adds a static function to a class which generates and returns  a list
-    of all its non-private members and stores them in a dictionary.
+    Adds a static function to a class which generates and returns a list of all its non-private members and stores them in a dictionary.
+    This should only be used for classes with static content, as the dictionary is generated only once.
     """
 
     @classmethod
     def todict(cls):
+        """
+        Returns a list of all of class cls' private members and caches them in a dict.
+
+        :type cls: Class
+        :rtype: dict
+        """
         try:
             return cls._cached_member_dict
         except AttributeError:
